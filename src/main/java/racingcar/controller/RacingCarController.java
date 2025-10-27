fcfc426deaee2dcfcbe3bc316663aee6ec7de167
ryulkim <hobby519@naver.com>
2025-10-27 20:41:26 +0900

    feat(output): 자동차 이름 안내 문구 출력

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,9 +1,11 @@
 package racingcar.controller;
 
 import racingcar.util.InputHandler;
+import racingcar.util.OutputView;
 
 public class RacingCarController {
     public void run(){
+        OutputView.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         InputHandler.readLine();
 
         InputHandler.close();
---
+++ src/main/java/racingcar/util/OutputView.java
@@ -0,0 +1,7 @@
+package racingcar.util;
+
+public class OutputView {
+    public static void print(String message) {
+        System.out.println(message);
+    }
+}
