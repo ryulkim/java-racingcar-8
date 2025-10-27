6e5b65b09168b5b9329f33c7360210204a5a280a
ryulkim <hobby519@naver.com>
2025-10-27 20:42:26 +0900

    feat(output): 횟수 입력 안내 문구 출력

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -7,6 +7,7 @@ public class RacingCarController {
     public void run(){
         OutputView.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         InputHandler.readLine();
+        OutputView.print("시도할 횟수는 몇 회인가요?");
 
         InputHandler.close();
     }
