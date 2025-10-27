7628ac6c4e6070fe9fd35f110d18f0c06c489124
ryulkim <hobby519@naver.com>
2025-10-27 20:35:50 +0900

    feat(input): 입력값 받기 기능 구현

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,9 @@
+package racingcar.controller;
+
+import racingcar.util.InputHandler;
+
+public class RacingCarController {
+    public void run(){
+        InputHandler.readLine();
+    }
+}
---
+++ src/main/java/racingcar/util/InputHandler.java
@@ -0,0 +1,9 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputHandler {
+    public static void readLine(){
+        Console.readLine();
+    }
+}
